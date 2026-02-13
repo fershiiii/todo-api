76ec66492e88f375a0a4da19be559bc82b89f8a4
Fernando Duran <duranfernando349@gmail.com>
2026-02-13 11:48:56 -0600

    feat: crear modelo task

---
+++ src/main/java/sv/edu/udb/todoapi/TodoApiApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/sv/edu/udb/todoapi/task/Task.java
@@ -0,0 +1,4 @@
+package sv.edu.udb.todoapi.task;
+
+public class Task {
+}
